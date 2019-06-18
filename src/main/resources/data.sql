53a8f54e8746f81c0f09d94e31d6bf30b0605118
Vedret.Hrvanovic <vedret.hrvanovic@ibm.com>
2019-06-18 20:32:56 +0200

    Initial commit. Created resources/data.sql with dummy data  to test  H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+create table users
+(
+id integer not null ,
+userName varchar (225),
+primary key (ID)
+);
+
+INSERT INTO users (id , userName)
+values (1001,'Vedret.Hrvanovic');
+
+INSERT INTO users (id , userName)
+values (1002,'Mak.Hrvanovic');
+
+INSERT INTO users (id , userName)
+values (1003,'Tom.Novy');
